d5e24b7b40f285efe83675ec4fd7912c2efc10c6
haoxuan <guohaoxuan@gmail.com>
2018-11-10 17:37:56 +0800

    fix

--- zaitakukanri.py
+++ zaitakukanri.py
@@ -44,6 +44,9 @@ for li in soup.findAll('li', {'class':'list-section'}):
     os.system(f"mkdir -p 'zaitakukanri_{src_id}'")
     os.system(f"curl 'https://www.zaitakukanri.co.jp/mediate/ajax/downloadzumenexecute/rentId/{src_id}/' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'DNT: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3606.0 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: https://www.zaitakukanri.co.jp/search/?&num=2000' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6,ja;q=0.5' -H 'Cookie: PHPSESSID=mmv5s2mqcvl9hma6je3vj7hmp5; _4fb564bf9277a72b766d6f3255e0f4b0=b5192df9438f397238a4b11a23e6be38' --compressed > zaitakukanri_{src_id}/doc.jpg")
 
+    if len(details['最寄り駅'].split('/')) < 2:
+        continue
+
     item["line"] = details['最寄り駅'].split('/')[0]
     item["station"] = details['最寄り駅'].split('/')[1]
     item["duration"] = details['駅より']
