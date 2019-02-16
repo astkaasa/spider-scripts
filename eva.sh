f4b03450a25c2a832d7bed378e43579377ff36f0
haoxuan <guohaoxuan@gmail.com>
2019-02-16 22:26:47 +0800

    update eva.sh

--- eva.sh
+++ eva.sh
@@ -1,5 +1,7 @@
 path=$(date +%F)
+mkdir "/home/ubuntu/data/dates/${path}"
+mkdir "/home/ubuntu/data/keys/${path}"
 ./run.sh
-./update.sh
+# ./update.sh
 zip -9 -r /home/ubuntu/data/zips/docs /home/ubuntu/data/docs
 zip -9 -r /home/ubuntu/data/zips/images /home/ubuntu/data/images
